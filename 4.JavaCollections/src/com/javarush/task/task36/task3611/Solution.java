53d649176a30599fcf07cb1d469fb051c13585ca
Nikolay Mukovozchik <n.mukovozchik@gmail.com>
2017-07-04 15:58:04 +0700

    Initial commit

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3611/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task36.task3611;
+
+import java.util.*;
+
+/* 
+Сколько у человека потенциальных друзей?
+*/
+
+public class Solution {
+    private boolean[][] humansRelationships;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.humansRelationships = generateRelationships();
+
+        Set<Integer> allFriendsAndPotentialFriends = solution.getAllFriendsAndPotentialFriends(4, 2);
+        System.out.println(allFriendsAndPotentialFriends);                              //expected: [0, 1, 2, 3, 5, 7]
+        Set<Integer> potentialFriends = solution.removeFriendsFromSet(allFriendsAndPotentialFriends, 4);
+        System.out.println(potentialFriends);                                           //expected: [2, 5, 7]
+    }
+
+    public Set<Integer> getAllFriendsAndPotentialFriends(int index, int deep) {
+        //напишите тут ваш код
+        Set<Integer> set = new HashSet<>();
+
+        set.add(index);
+        while(deep > 0){
+            List<Integer> list = new ArrayList<>();
+            for (Iterator<Integer> it = set.iterator(); it.hasNext(); ) {
+                Integer ii = it.next();
+                for (int i = 0; i < humansRelationships.length; i++) {
+                    if ((i < ii) && (ii < humansRelationships.length) && humansRelationships[ii][i]) {
+                        list.add(i);
+                    } else if ((i > ii) && humansRelationships[i][ii]) {
+                        list.add(i);
+                    }
+                }
+            }
+            set.addAll(list);
+            deep--;
+        }
+        set.remove(index);
+        return set;
+    }
+
+    //remove people from set, with which you have already had relationship
+    public Set<Integer> removeFriendsFromSet(Set<Integer> set, int index) {
+        for (int i = 0; i < humansRelationships.length; i++) {
+            if ((i < index) && (index < humansRelationships.length) && humansRelationships[index][i]) {
+                set.remove(i);
+            } else if ((i > index) && humansRelationships[i][index]) {
+                set.remove(i);
+            }
+        }
+        return set;
+    }
+
+    //return test data
+    private static boolean[][] generateRelationships() {
+        return new boolean[][]{
+                {true},                                                                 //0
+                {true, true},                                                           //1
+                {false, true, true},                                                    //2
+                {false, false, false, true},                                            //3
+                {true, true, false, true, true},                                        //4
+                {true, false, true, false, false, true},                                //5
+                {false, false, false, false, false, true, true},                        //6
+                {false, false, false, true, false, false, false, true}                  //7
+        };
+    }
+}
